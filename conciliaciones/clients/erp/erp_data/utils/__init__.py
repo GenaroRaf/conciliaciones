c8de00346cadda823334465c6f464bf86fc55a76
Genaro Rafael Fonseca Amaro <genaro.fonseca@kuantik.mx>
2025-11-19 13:23:56 -0600

    [FEAT:update] Se actualizó importación para evitar importaión ciclica
    - Se actualizó la impotación de ValidaDataTypes

--- conciliaciones/clients/erp/erp_data/utils/__init__.py
+++ conciliaciones/clients/erp/erp_data/utils/__init__.py
@@ -1,3 +1,3 @@
-from conciliaciones.clients.erp.erp_data.utils import ValidaDataTypes
+from conciliaciones.clients.erp.erp_data.utils.data_types import ValidaDataTypes
 
 __all__: list[str] = ["ValidaDataTypes"]
